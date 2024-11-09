b7a54b2e78d78c76c49672e57f12d01da86e2858
GhofraneBenzaamia <benzaamiaghofrane@gmail.com>
2024-11-09 19:31:41 +0100

    Your commit message here

--- tp2.pl
+++ tp2.pl
@@ -1,8 +1,12 @@
+%QUESTION 1
 % Base case: X is the head of the list, so X is an element of the list.
 element_of(X, [X | _]).
 
 % Recursive case: X is an element of the tail if X is not the head.
 element_of(X, [_ | Tail]) :-
     element_of(X, Tail).
+
+%QUESTION 2
+first(E, [E | _]).
 
 
