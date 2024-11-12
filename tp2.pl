b10d496a22f245be1993092335a26297d4e5a07f
Belkais <tba19022@gmail.com>
2024-11-12 14:26:37 +0100

    6 to 9

--- tp2.pl
+++ tp2.pl
@@ -9,7 +9,6 @@ element_of(X, [_ | Tail]) :-
 %QUESTION 2
 first(E, [E | _]).
 
-
 %QUESTION 3
 % Base case: the last element of a single-element list is that element itself.
 last([X], X).
@@ -35,3 +34,42 @@ del_k(X, [Head|Tail], K, [Head|NewTail]) :-
     K > 1,
     K1 is K - 1,
     del_k(X, Tail, K1, NewTail).
+
+
+%QUESTION 6
+% Base case: an empty list has length 0.
+length([], 0).
+
+% Recursive case: the length of the list is 1 plus the length of the tail.
+length([_|Tail], N) :-
+    length(Tail, N1),
+    N is N1 + 1.
+
+%QUESTION 7
+% Base case: an empty list has an even number of elements.
+even([]).
+
+% Recursive case: if we can remove two elements, the list remains even.
+even([_, _|Tail]) :-
+    even(Tail).
+
+%QUESTION 8
+% Base case: concatenating an empty list with L gives L.
+concat([], L, L).
+
+% Recursive case: add the head of L1 to the result of concatenating the tail.
+concat([Head|Tail], L2, [Head|L3]) :-
+    concat(Tail, L2, L3).
+
+%QUESTION 9
+% A list is a palindrome if it is equal to its reverse.
+palindrome(L) :-
+    reverse(L, L).
+
+% Base case for reverse.
+reverse([], []).
+
+% Recursive case: add the head to the reversed tail.
+reverse([Head|Tail], RevL) :-
+    reverse(Tail, RevTail),
+    concat(RevTail, [Head], RevL).
